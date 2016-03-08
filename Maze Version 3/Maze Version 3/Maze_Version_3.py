531fbb15395e5e0c8f9444c29dd8992fbdec7960
Mike Aspinwall <startrekrules4@gmail.com>
2016-03-08 14:43:52 -0500

    mazev3_with_workingreader

--- Maze Version 3/Maze Version 3/Maze_Version_3.py
+++ Maze Version 3/Maze Version 3/Maze_Version_3.py
@@ -13,26 +13,15 @@ def playerinput():
         return move
     else:
         print ("Please Enter Valid Input")
-        
- #Array takes form [y][x] <----(Why?!)
-grid = [["H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H"], #0
-             ["H", "P", "_", "_", "_", "_", "_", "_", "_", "_", "_", "H"], #1
-             ["H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "_", "H"], #2
-             ["H", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "H"], #3
-             ["H", "_", "H", "H", "H", "_", "H", "H", "H", "H", "H", "H"], #4
-             ["H", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "H"], #5
-             ["H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "_", "H"], #6
-             ["H", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "H"], #7
-             ["H", "_", "H", "H", "H", "H", "H", "H", "H", "H", "_", "H"], #8
-             ["H", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "H"], #9
-             ["H", "K", "H", "H", "H", "_", "H", "H", "H", "H", "H", "H"], #10
-             ["H", "_", "_", "_", "_", "_", "D", "_", "_", "_", "F", "H"], #11
-             ["H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H"]] #12
-             #0    #1   #2   #3   #4   #5   #6   #7   #8   #9   #10  #11
-
 
 #Fog: Display 0s around player or make view smaller 
+x = open("level1.txt")
 
+def reader(x):
+    for line in x:
+        print(line)
+
+grid = reader(x)
 
 def haskey(a):
     global key
@@ -103,8 +92,7 @@ def iswin(a):
         return True
     else: 
         return False
-        
-        
+                
 def game(board1):
     x = 1
     y = 1
